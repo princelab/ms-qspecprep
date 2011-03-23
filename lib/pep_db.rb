03883f98149788ad1023ab3337b56bdd00c105e8
Ryan Taylor <ryanmt@byu.net>
2011-03-23 12:09:50 -0600

    first algorithm for pep_db is done, but it is too slow, 33 hrs to finish the human DB

--- lib/pep_db.rb
+++ lib/pep_db.rb
@@ -2,35 +2,39 @@
 
 MaxMissedCleavages = 3
 
-Protein = Struct.new(:peptides, :id, :entry_name, :protein_name, :organism_name, :gene_name, :sequence)
-Peptide = Struct.new(:sequence, :protein_ids)
+Protein = Struct.new(:peptides, :id, :entry_name, :protein_name, :organism_name, :sequence)
+#		Peptide = Struct.new(:sequence, :protein_ids)  ### Replaced by a hash
 
 def digester(string, missed_cleavages) # Returns an array of chomped sequences
+	string.upcase!
 	arr = (0..(string.upcase.length-1)).map {|i| string[i]}
-	last, curr, next_item = nil, nil, nil
-	misses = 0
-	splits = []
-	keeper = ""
-	arr.each_index do |i|
-		last = arr[i-1]; curr = arr[i]; next_item = arr[i+1]
-#		puts "last, curr, next: #{last}, #{curr}, #{next_item}"
-		keeper << curr if curr
-#		puts "keeper: #{keeper}"
-		if curr == 'R' or curr == 'K'
-			break if next_item == "P"
-			#puts "misses:missed cleavages			#{misses}:#{missed_cleavages}"
-			if misses < missed_cleavages
-				misses += 1
-			else
-#				puts 'cutting'
-				splits << keeper
-				keeper = ""
-				misses = 0
+	misses = 0; splits = []
+	(0..missed_cleavages).each do |miss_cleav|
+		(0..missed_cleavages).each do |init_num|
+			last, curr, next_item = nil, nil, nil; keeper = ""
+			misses =  init_num
+			arr.each_index do |i|
+				last = arr[i-1]; curr = arr[i]; next_item = arr[i+1]
+		#	puts "last, curr, next: #{last}, #{curr}, #{next_item}"
+				keeper << curr if curr
+		#	puts "keeper: #{keeper}"
+				if curr == 'R' or curr == 'K'
+					unless next_item == "P"
+		#				puts "misses:missed cleavages			#{misses}:#{miss_cleav}"
+						if misses < miss_cleav
+							misses += 1
+						else
+							splits << keeper
+							keeper = "";	misses = 0
+						end
+					end
+				end
+				splits << keeper if next_item == nil
+		#		puts "Splits looks like: #{splits}"
 			end
-		end
-		splits << keeper if next_item == nil
-	end			
-	splits
+		end			
+	end
+	splits.uniq
 end
 
 
@@ -54,40 +58,40 @@ class PepDigest
 				end
 				@proteins.last.sequence = @proteins.last.sequence + line.chomp
 			else
-				id, entry_name, protein_name, organism_name, gene_name = /^>sp\|(\w*-?\w*)\|(\w*)\s(.*)\sOS=(.*)\sGN=(\w+).*/.match(line).captures
-				@proteins << Protein.new(nil, id, entry_name, protein_name, organism_name, gene_name, nil)
+			begin
+				id, entry_name, protein_name, organism_name = /^>[a-z]{2}\|(\w*-?\w*)\|(\w*)\s(.*)\sOS=(\w+\s?\w+).*/.match(line).captures
+			rescue NoMethodError , TypeError=> e
+				print "\nError reading line: \n #{line} \n Error message: #{e}"
+				print "Matches looks like: " + /^>[a-z]{2}\|(\w*-?\w*)\|(\w*)\s(.*)\sOS=(\w+\s?\w+).*/.match(line)
+			end
+				@proteins << Protein.new([], id, entry_name, protein_name, organism_name, nil)
 			end
 		end
 	end
 	def digest
-		@peptides = []
-		tmp_peps = []
+	puts "\ndigesting"
+		@peptides = {}
 		@proteins.each do |prot|
 			seq = prot.sequence
 			(0..MaxMissedCleavages).each do |missed_cleav|
 				digester(seq, missed_cleav).each do |pept|
-					tmp_peps << Peptide.new(pept, [prot.id])
+					if @peptides[pept.to_sym].nil?
+						@peptides[pept.to_sym] = [prot.id]
+					else
+						@peptides[pept.to_sym] = @peptides[pept.to_sym] << prot.id
+						@peptides[pept.to_sym].uniq!
+					end
+					prot.peptides << pept
 #			 ###		Peptide = Struct.new(:sequence, :protein_ids)
 				end
 			end
  		end
-		tmp_peps = tmp_peps.sort_by{|a| [a.sequence]}
-		prev_p = nil
-		tmp_peps.each_index do |i|
-			prev_p = tmp_peps[i-1]
-			p = tmp_peps[i]
-			if prev_p.sequence == p.sequence
-				p.protein_ids << prev_p.protein_ids
-				tmp_peps[i-1] = nil
-				p.protein_ids.flatten!.sort!.uniq!
-				p p.protein_ids
-			end
-		end
+#		p @peptides
+	end
 		
 			
 
 #		HOW Do I combine the duplicates by appending the protein tag to the list of peptides?  
 			## Do I need to keep track of the missed cleavages?  Can't I just calculate that later, if I ever want it, with less overhead and faster than moving them around the entire time?  TRYING IT@!
-	end
 end
 
--- spec/pepdb_spec.rb
+++ spec/pepdb_spec.rb
@@ -6,36 +6,58 @@ describe "Peptide_DB" do
 		@digest = PepDigest.new(tfasta)
 		@digest.digest
 	end
+	it 'Times required for sets of test files...' do 
+		tfasta = TESTFILE + 'test.fasta'
+		tfasta1 = TESTFILE + 'test1.fasta'
+		tfasta2 = TESTFILE + 'test2.fasta'
+		tfasta3 = TESTFILE + 'test3.fasta'
+		t = Time.now
+		@digest = PepDigest.new(tfasta)
+		@digest.digest
+		puts "-------------------\nThe smallest file took #{Time.now-t}\n--------------------"		
+		t = Time.now
+		@digest = PepDigest.new(tfasta1)
+		@digest.digest
+		puts "-------------------\nThe small test file took #{Time.now-t}\n--------------------"
+		t = Time.now
+		@digest = PepDigest.new(tfasta2)
+		@digest.digest
+		puts "-------------------\nThe medium test file took #{Time.now-t}\n--------------------"
+#		t = Time.now
+#		@digest = PepDigest.new(tfasta3)
+#		@digest.digest
+#		puts "-------------------\nThe largest test file took #{Time.now-t}\n--------------------"
+	end
 	it "Parses Uniprot FASTA correctly (ID)" do 
 		@digest.proteins.first.id.should.equal "P31946"
 	end
-=begin
+	it 'Digester fxn checks out as accurate' do 
+		digester('RARPRANDKLAM', 0).class.should.equal Array.new.class
+		digester('RARPRANDKLAM', 0).should.equal ["R", "ARPR", "ANDK", "LAM"]
+		digester('RARPRANDKLAM', 1).should.equal ["R", "ARPR", "ANDK", "LAM", "RARPR", "ANDKLAM", "ARPRANDK"]
+	end
+#	it 'works on a large fasta' do 
+#		t = Time.now
+#		@fullfile = PepDigest.new('/home/ryanmt/lab/DB/uni_human_var_100517_fwd.fasta')
+#		@fullfile.digest
+#		puts "-------------------\nTime required was #{Time.now-t}\n--------------------"
+#	end
 	it "Parses Uniprot FASTA correctly (EntryName)" do
-		@digest.proteins.first.tmp  
+		@digest.proteins.first.entry_name.should.equal '1433B_HUMAN'
 	end
 	it "Parses Uniprot FASTA correctly (ProteinName)" do
-		@digest.proteins.first.tmp
+		@digest.proteins.first.protein_name.should.equal "14-3-3 protein beta/alpha"
 	end
 	it "Parses Uniprot FASTA correctly (OrganismName)" do 
-		@digest.proteins.first.tmp
-	end
-	it "Parses Uniprot FASTA correctly (GeneName)" do 
-		@digest.proteins.first.tmp
-	end
-	it "Parses Uniprot FASTA correctly (ProteinExistence)" do 
-		@digest.proteins.first.tmp
-	end
-	it "Parses Uniprot FASTA correctly (SequenceVersion)" do 
-		@digest.proteins.first.tmp
+		@digest.proteins.first.organism_name.should.equal "Homo sapiens"
 	end
 	it "Parses Uniprot FASTA correctly (Sequence)" do 
-		@digest.proteins.first.tmp
+		@digest.proteins.first.sequence.should.equal "MTMDKSELVQKAKLAEQAERYDDMAAAMKAVTEQGHELSNEERNLLSVAYKNVVGARRSSWRVISSIEQKTERNEKKQQMGKEYREKIEAELQDICNDVLELLDKYLIPNATQPESKVFYLKMKGDYFRYLSEVASGDNKQTTVSNSQQAYQEAFEISKKEMQPTHPIRLGLALNFSVFYYEILNSPEKACSLAKTAFDEAIAELDTLNEESYKDSTLIMQLLRDNLTLWTSENQGDEGDAGEGEN"
 	end
-	it "Digests all proteinss into their peptide fragments" do 
-		@digest.proteins.first.peptides.should.be Array
+	it "Digests all proteins into their peptide fragments" do 
+		@digest.proteins.first.peptides.class.should.equal Array.new.class
 	end
 	it "Lists all peptides with the proteins from which it could id" do 
-		@digest.proteins.first.pep_db.should.be Hash
+		@digest.peptides.first.should.equal ["MTMDK".to_sym, ["P31946"]]
 	end
-=end
 end
